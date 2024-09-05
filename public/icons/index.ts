fe193687b4666c91c7f9747ed6d6d3051a8480a0
chaeney <chaekey@gmail.com>
2024-09-05 15:56:28 +0900

    style: svg 컴포넌트화 및 svg 사용 관련 설정

---
+++ public/icons/index.ts
@@ -0,0 +1,41 @@
+import IconAlarm from './alarm.svg';
+import IconArrow from './arrow.svg';
+import IconBye from './bye.svg';
+import IconCaret from './caret.svg';
+import IconCheck from './check.svg';
+import IconCheckBoxDefault from './checkbox-default.svg';
+import IconCheckBoxActive from './checkbox-active.svg';
+import IconDalaemfit from './dalaemfit.svg';
+import IconHeart from './heart.svg';
+import IconX from './icon-x.svg';
+import IconPerson from './person.svg';
+import IconSaveActive from './save-active.svg';
+import IconSaveDiscardBtn from './save-discard-btn.svg';
+import IconSaveDiscard from './save-discard.svg';
+import IconSaveInactive from './save-inactive.svg';
+import IconSort from './sort.svg';
+import IconVisivilityOff from './visivility-off.svg';
+import IconVisivilityOn from './visivility-on.svg';
+import IconWorkation from './workation.svg';
+
+export {
+  IconAlarm,
+  IconArrow,
+  IconBye,
+  IconCaret,
+  IconCheck,
+  IconCheckBoxDefault,
+  IconCheckBoxActive,
+  IconDalaemfit,
+  IconHeart,
+  IconX,
+  IconPerson,
+  IconSaveActive,
+  IconSaveDiscardBtn,
+  IconSaveDiscard,
+  IconSaveInactive,
+  IconSort,
+  IconVisivilityOff,
+  IconVisivilityOn,
+  IconWorkation,
+};
