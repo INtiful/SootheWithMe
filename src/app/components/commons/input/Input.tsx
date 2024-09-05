75a1cbd3685a0ecfff326039e866ad8d79349c36
hakyoung12 <gkr097@naver.com>
2024-09-05 17:23:04 +0900

    feat: input 컴포넌트 추가

---
+++ src/app/components/commons/input/Input.tsx
@@ -0,0 +1,37 @@
+'use client';
+import { forwardRef } from 'react';
+
+const InputStyles = {
+  base: 'w-full items-center gap-4 rounded-lg px-16 py-8 hover:ring-2 hover:ring-var-orange-300 focus:ring-2 focus:ring-var-orange-600',
+  error: 'ring-var-red ring-2', // form 사용시 에러메시지용 레이아웃
+};
+
+/**
+ * Input 컴포넌트의 props 타입 정의
+ * @property {boolean} [hasError=false] - 에러 상태를 나타내는 선택적 prop
+ * @property {string} [className=''] - 추가적인 사용자 정의 클래스 이름
+ */
+interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
+  hasError?: boolean;
+  className?: string;
+}
+
+/**
+ * Input 컴포넌트 정의
+ *
+ * @param {Props} props - Input 컴포넌트에 전달되는 props
+ * @param {Ref<HTMLInputElement>} ref - forwardRef를 사용하여 전달받은 ref
+ */
+const Input = forwardRef<HTMLInputElement, Props>(
+  ({ hasError = false, className = '', ...rest }, ref) => {
+    return (
+      <input
+        ref={ref}
+        className={`${InputStyles.base} ${hasError && InputStyles.error} ${className}`}
+        {...rest}
+      />
+    );
+  },
+);
+
+export default Input;
