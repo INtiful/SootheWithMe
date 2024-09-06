a2ad7ec6e6b38de2baf850ed21abf427c25299d3
chaeney <chaekey@gmail.com>
2024-09-06 14:38:49 +0900

    [KAN-3] feat:  컴포넌트 Tab 추가 및 svg icon 코드 변경

---
+++ src/app/components/Tab/Tab.tsx
@@ -0,0 +1,32 @@
+import { IconDalaemfit, IconWorkation } from '@/public/icons';
+
+interface TabProps {
+  type: 'workation' | 'dalaemfit';
+  isActive: boolean;
+  onClick: () => void;
+}
+
+const Tab = ({ type, isActive, onClick }: TabProps) => {
+  const styles = isActive
+    ? 'text-var-gray-900 first-of-type:before:right-0 last-of-type:before:left-0'
+    : 'text-var-gray-400 first-of-type:before:-right-full last-of-type:before:-left-full';
+
+  return (
+    <div
+      className={`relative flex items-center gap-4 overflow-hidden px-4 pb-4 text-18 font-semibold transition ${styles} before:absolute before:bottom-0 before:w-full before:border-b-2 before:border-var-gray-900 before:transition-all before:duration-300`}
+      onClick={onClick}
+    >
+      {type === 'workation' ? (
+        <>
+          워케이션 <IconWorkation width={32} height={32} />
+        </>
+      ) : (
+        <>
+          달램핏 <IconDalaemfit width={32} height={32} />
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Tab;
