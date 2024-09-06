641109c6849554025840f1691e25791206636f95
chaeney <chaekey@gmail.com>
2024-09-06 14:39:33 +0900

    [KAN-3] feat:  컴포넌트 Badge, Tag 추가

---
+++ src/app/components/Badge/Badge.tsx
@@ -0,0 +1,15 @@
+interface BadgeProps {
+  children: string;
+}
+
+const Badge = ({ children }: BadgeProps) => {
+  return (
+    <>
+      <div className='rounded-full bg-var-gray-900 px-8 text-12 font-semibold text-white'>
+        {children}
+      </div>
+    </>
+  );
+};
+
+export default Badge;
