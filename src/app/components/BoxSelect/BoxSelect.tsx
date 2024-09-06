2f8013eeb088aff51f27cfedd6116c698bf59587
Matthew Song <songww1997@naver.com>
2024-09-06 13:32:02 +0900

    [KAN-11] style: BoxSelect 컴포넌트 (모바일 버전) 추가 및 trasition 추가

--- src/app/components/BoxSelect/BoxSelect.tsx
+++ src/app/components/BoxSelect/BoxSelect.tsx
@@ -22,15 +22,15 @@ const BoxSelect = ({ title = '', subTitle = '' }: BoxSelectProps) => {
 
   return (
     <div
-      className={`h-70 flex w-full items-start gap-8 rounded-lg ${selectStatus ? 'bg-var-gray-900 text-var-white' : 'bg-var-gray-50 text-var-black'} px-16 py-12`}
+      className={`h-70 flex w-full items-start gap-8 rounded-lg ${selectStatus ? 'bg-var-gray-900 text-var-white' : 'bg-var-gray-50 text-var-black'} px-8 py-[6px] transition-colors duration-200 ease-in-out md:lg:py-12 md:lg:px-16`}
     >
       <input
         type='checkbox'
-        className="mt-4 h-24 w-24 appearance-none rounded-md bg-[url('/icons/checkbox-default.svg')] bg-center bg-no-repeat checked:bg-[url('/icons/checkbox-active.svg')] checked:bg-center checked:bg-no-repeat"
+        className="h-24 w-24 appearance-none rounded-md bg-[url('/icons/checkbox-default.svg')] bg-center bg-no-repeat checked:bg-[url('/icons/checkbox-active.svg')] checked:bg-center checked:bg-no-repeat"
         onChange={handleCheckboxChange}
       />
       <div className='flex flex-col'>
-        <div className='text-[16px] font-bold'>{title}</div>
+        <div className='text-[14px] font-bold md:lg:text-[16px]'>{title}</div>
         <div className='text-[12px] font-medium'>{subTitle}</div>
       </div>
     </div>
