830cee4f37ba394781bd490d480aea8a883dbbf4
Matthew Song <songww1997@naver.com>
2024-09-06 14:02:19 +0900

    [KAN-11] fix: 상위 태그를 label 태그로 교체

--- src/app/components/BoxSelect/BoxSelect.tsx
+++ src/app/components/BoxSelect/BoxSelect.tsx
@@ -21,19 +21,22 @@ const BoxSelect = ({ title = '', subTitle = '' }: BoxSelectProps) => {
   };
 
   return (
-    <div
-      className={`h-70 flex w-full items-start gap-8 rounded-lg ${selectStatus ? 'bg-var-gray-900 text-var-white' : 'bg-var-gray-50 text-var-black'} px-8 py-[6px] transition-colors duration-200 ease-in-out md:lg:py-12 md:lg:px-16`}
+    <label
+      className={`h-70 flex w-full items-start gap-8 rounded-lg ${selectStatus ? 'bg-var-gray-900 text-var-white' : 'bg-var-gray-50 text-var-black'} px-8 py-[6px] transition-colors duration-200 ease-in-out md:px-16 md:py-12 lg:px-16 lg:py-12`}
     >
       <input
+        id='checkbox'
         type='checkbox'
         className="h-24 w-24 appearance-none rounded-md bg-[url('/icons/checkbox-default.svg')] bg-center bg-no-repeat checked:bg-[url('/icons/checkbox-active.svg')] checked:bg-center checked:bg-no-repeat"
         onChange={handleCheckboxChange}
       />
       <div className='flex flex-col'>
-        <div className='text-[14px] font-bold md:lg:text-[16px]'>{title}</div>
+        <div className='text-[14px] font-bold md:text-[16px] lg:text-[16px]'>
+          {title}
+        </div>
         <div className='text-[12px] font-medium'>{subTitle}</div>
       </div>
-    </div>
+    </label>
   );
 };
 
