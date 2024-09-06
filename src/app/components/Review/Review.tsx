a8ea6240b168b744db07e9c6ec4524aff103ba3e
Homin <coc1381@naver.com>
2024-09-06 15:19:55 +0900

    [KAN-13] fix: Review 컴포넌트 하나로 병합

---
+++ src/app/components/Review/Review.tsx
@@ -0,0 +1,69 @@
+import Image from 'next/image';
+
+import { IconHeart } from '@/public/icons';
+import { Profile } from '@/public/images';
+
+interface ReviewProps {
+  image_source?: string;
+  rating: number;
+  description: string;
+  place?: string;
+  location?: string;
+  user_name: string;
+  date: string;
+}
+
+const Review = ({
+  image_source,
+  rating,
+  description,
+  place,
+  location,
+  user_name,
+  date,
+}: ReviewProps) => {
+  return (
+    <div className='flex-col space-x-6 p-[10px] font-medium md:flex md:flex-row'>
+      {image_source ? (
+        <div>
+          <Image
+            className='mx-auto w-full rounded-[24px] p-[10px] md:p-0'
+            src={image_source}
+            alt='review image'
+            width={378}
+            height={200}
+            quality={85}
+          />
+        </div>
+      ) : null}
+
+      <div>
+        <div className='mt-[10px] flex md:mt-0'>
+          {Array.from({ length: rating }).map((_, index) => (
+            <IconHeart key={index} className='h-24 w-24 text-var-orange-600' />
+          ))}
+        </div>
+
+        <div className='mt-[10px] text-[14px] text-black'>
+          <div>{description}</div>
+        </div>
+
+        <div className='mt-[10px] text-var-gray-700'>
+          {place} 이용 · {location}
+        </div>
+
+        <div className='mt-8 flex items-center space-x-[8px] divide-x divide-var-gray-700 md:mt-10'>
+          <div>
+            <div className='flex items-center space-x-[8px]'>
+              <Profile className='h-24 w-24' />
+              <div className='text-[12px] text-var-gray-700'>{user_name}</div>
+            </div>
+          </div>
+          <div className='pl-[12px] text-[12px] text-var-gray-500'>{date}</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Review;
