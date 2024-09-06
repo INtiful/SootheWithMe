32fe00b7ca8e90a7dac8581d2b550c1fd8c193d3
hakyoung12 <gkr097@naver.com>
2024-09-06 10:16:51 +0900

    [KAN-5] style: InputStyle 수정

--- src/app/components/commons/input/Input.tsx
+++ src/app/components/commons/input/Input.tsx
@@ -3,7 +3,9 @@ import { forwardRef } from 'react';
 
 // @todo: 추후 파일 분리예정
 export const InputStyles = {
-  base: 'w-full items-center gap-4 rounded-lg px-16 py-8 hover:ring-2 hover:ring-var-orange-300 focus:ring-2 focus:ring-var-orange-600',
+  base: 'w-full items-center gap-4 rounded-lg px-16 py-8 focus:ring-2',
+  hover: 'hover:ring-2 hover:ring-var-orange-300',
+  focus: 'focus:ring-var-orange-600',
   error: 'ring-var-red ring-2', // form 사용시 에러메시지용 레이아웃
 };
 
@@ -20,7 +22,7 @@ interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
 /**
  * Input 컴포넌트 정의
  *
- * @param {Props} props - Input 컴포넌트에 전달되는 props
+ * @param {InputProps} props - Input 컴포넌트에 전달되는 props
  * @param {Ref<HTMLInputElement>} ref - forwardRef를 사용하여 전달받은 ref
  */
 const Input = forwardRef<HTMLInputElement, Props>(
@@ -28,7 +30,7 @@ const Input = forwardRef<HTMLInputElement, Props>(
     return (
       <input
         ref={ref}
-        className={`${InputStyles.base} ${hasError && InputStyles.error} ${className}`}
+        className={`${InputStyles.base} hover: ${InputStyles.hover} focus: ${InputStyles.focus} ${hasError && InputStyles.error} ${className}`}
         {...rest}
       />
     );
