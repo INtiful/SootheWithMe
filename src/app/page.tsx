0a2facd5b72b2f7882928b9f49404e81a44fc05e
Matthew Song <songww1997@naver.com>
2024-09-06 13:32:02 +0900

    [KAN-11] style: BoxSelect 컴포넌트 (모바일 버전) 추가 및 trasition 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import BoxSelect from './components/BoxSelect/BoxSelect';
 export default function Home() {
   return (
     <main className='flex h-dvh w-dvw items-center justify-center bg-white'>
-      <div className='flex h-100 w-472 gap-12 bg-var-orange-200'>
+      <div className='flex h-100 w-472 gap-12 bg-var-white'>
         <BoxSelect title={'달램핏'} subTitle={'오피스 트레이닝'} />
         <BoxSelect title={'달램핏'} subTitle={'마인드풀니스'} />
         <BoxSelect title={'워케이션'} subTitle={''} />
