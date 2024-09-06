4be38c807a4f80b371e1cc882242369b74b402cd
Matthew Song <songww1997@naver.com>
2024-09-06 17:31:17 +0900

    [KAN-11] fix: 변수명 변경 및 테일윈드 수정

--- src/app/components/BoxSelect/BoxSelect.tsx
+++ src/app/components/BoxSelect/BoxSelect.tsx
@@ -14,15 +14,15 @@ interface BoxSelectProps {
 }
 
 const BoxSelect = ({ title = '', subTitle = '' }: BoxSelectProps) => {
-  const [selectStatus, setSelectStatus] = useState<boolean>(false);
+  const [isSelected, setIsSelected] = useState<boolean>(false);
 
   const handleCheckboxChange = (event: ChangeEvent<HTMLInputElement>) => {
-    setSelectStatus(event.target.checked);
+    setIsSelected(event.target.checked);
   };
 
   return (
     <label
-      className={`flex h-76 w-full items-start gap-8 rounded-lg ${selectStatus ? 'bg-var-gray-900 text-var-white' : 'bg-var-gray-50 text-var-black'} md:h-70 lg:h-70 px-8 py-[6px] transition-colors duration-200 ease-in-out md:px-16 md:py-12 lg:px-16 lg:py-12`}
+      className={`flex h-76 w-full items-start gap-8 rounded-lg ${isSelected ? 'bg-var-gray-900 text-var-white' : 'bg-var-gray-50 text-var-black'} md:h-70 lg:h-70 px-8 py-[6px] transition-colors duration-200 ease-in-out md:px-16 md:py-12`}
     >
       <input
         id='checkbox'
@@ -31,10 +31,8 @@ const BoxSelect = ({ title = '', subTitle = '' }: BoxSelectProps) => {
         onChange={handleCheckboxChange}
       />
       <div className='flex flex-col'>
-        <div className='text-[14px] font-bold md:text-[16px] lg:text-[16px]'>
-          {title}
-        </div>
-        <div className='text-[12px] font-medium'>{subTitle}</div>
+        <div className='text-14 md:text-16 font-bold'>{title}</div>
+        <div className='text-12 font-medium'>{subTitle}</div>
       </div>
     </label>
   );
