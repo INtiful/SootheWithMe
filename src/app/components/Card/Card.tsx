0b04b1cc47f321ce6682aa9a9bb59d37d4cc91ac
chaeney <chaekey@gmail.com>
2024-09-08 15:36:44 +0900

    [KAN-16] feat : Card 컴포넌트 추가

---
+++ src/app/components/Card/Card.tsx
@@ -0,0 +1,152 @@
+import {
+  IconPerson,
+  IconSaveDiscard,
+  IconSaveDiscardBtn,
+} from '@/public/icons';
+import Image from 'next/image';
+import Button from '../Button/Button';
+import StateChip from '../Chip/StateChip';
+import { formatDate, formatTime } from '@/utils/formatDate';
+
+interface GatheringsData {
+  teamId: number;
+  id: number;
+  type: string;
+  name: string;
+  dateTime: string;
+  registrationEnd: string;
+  location: string;
+  participantCount: number;
+  capacity: number;
+  image: string;
+  createdBy: number;
+  canceledAt?: string;
+  joinedAt: string;
+  isCompleted: boolean;
+  isReviewed: boolean;
+}
+
+interface CardProps {
+  data: GatheringsData;
+  hasButton?: boolean;
+  hasChips?: boolean;
+  handleCancelGatherings?: () => void; // 예약 취소 버튼 클릭 핸들러
+  handleWriteReview?: () => void; // 리뷰 작성 버튼 클릭 핸들러
+}
+
+const Card = ({ data, hasButton, hasChips }: CardProps) => {
+  // Button 렌더링
+  const button = () => {
+    if (!data.isCompleted) {
+      return (
+        <Button
+          type='button'
+          name='예약 취소하기'
+          variant='white'
+          //   onClick={handleCancelGatherings}
+        />
+      );
+    }
+    if (data.isCompleted && !data.isReviewed) {
+      return (
+        <Button
+          type='button'
+          name='리뷰 작성하기'
+          variant='default'
+          //   onClick={handleWriteReview}
+        />
+      );
+    }
+    return null;
+  };
+
+  // State chip 렌더링
+  const chips = () => {
+    // 이용 완료 시
+    if (data.isCompleted) {
+      return <StateChip state='done' />;
+    }
+    if ('이용 예정, 개설 확정 로직') {
+      return (
+        <>
+          <StateChip state='scheduled' />
+          <StateChip state='confirmed' />
+        </>
+      );
+    }
+    if ('이용 예정, 개설 대기 로직') {
+      return (
+        <>
+          <StateChip state='scheduled' />
+          <StateChip state='pending' />
+        </>
+      );
+    }
+  };
+
+  return (
+    <div className='w-full border-b-2 border-dashed border-var-gray-200 py-24'>
+      <div className='relative flex w-full flex-col gap-16 overflow-hidden rounded-[12px] md:flex-row md:rounded-[24px]'>
+        {/* 이미지 */}
+        <div className='md: relative flex h-156 w-full flex-col gap-16 md:w-280'>
+          <Image
+            className='rounded-[24px] object-cover'
+            src={data.image}
+            alt='image'
+            fill
+            quality={85}
+            sizes='(max-width: 768px) 100vw, 378px'
+          />
+        </div>
+
+        {/* 정보 */}
+        <div className='flex flex-col gap-[6px] p-2'>
+          {/* StateChip이 있는 경우 */}
+          {hasChips && <div className='mb-[6px] flex gap-8'>{chips()}</div>}
+          <div className='flex items-center gap-8 text-18 font-semibold text-var-gray-900'>
+            <p>{data.name}</p>
+            <span>|</span>
+            <p className='text-14 font-medium text-var-gray-700'>
+              {data.location}
+            </p>
+          </div>
+          <div className='flex items-center gap-4 text-14 font-medium text-var-gray-700'>
+            <p>{formatDate(data.dateTime)}</p>
+            <span>·</span>
+            <p>{formatTime(data.dateTime)}</p>
+            <IconPerson className='ml-8 h-16 w-16' />
+            <p>
+              {data.participantCount}/{data.capacity}
+            </p>
+          </div>
+
+          {/* 버튼이 있는 경우 */}
+          {hasButton && (
+            <div className='mt-12 w-120 md:mt-auto'>{button()}</div>
+          )}
+        </div>
+
+        {/* 취소된 모임의 경우 */}
+        {data.canceledAt && (
+          <div className='absolute left-0 top-0 z-10 flex h-full w-full flex-col items-center justify-center bg-var-black bg-opacity-80'>
+            <p className='text-14 font-medium text-white'>
+              모집 취소된 모임이에요.
+              <br />
+              다음 기회에 만나요 🙏
+            </p>
+            <button
+              type='button'
+              className='right-24 mt-24 md:absolute md:top-24 md:mt-0'
+              // TODO : onClick={}
+            >
+              <IconSaveDiscardBtn className='h-36 w-116 md:hidden' />
+              <IconSaveDiscard className='hidden h-48 w-48 md:block' />
+            </button>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Card;
