471e87c3ddbdcc8e28945aabf0c73d06020ce494
chaeney <chaekey@gmail.com>
2024-09-08 15:36:02 +0900

    style : font 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,6 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
 import '@/styles/globals.css';
 import '@/styles/reset.css';
-
-const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -17,7 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='ko'>
-      <body className={inter.className}>{children}</body>
+      <body className='font-pretendard'>{children}</body>
     </html>
   );
 }
