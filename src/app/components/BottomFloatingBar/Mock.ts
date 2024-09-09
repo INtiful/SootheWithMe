2a13544466ec1dc810f4846c16fab148c2010a3a
hakyoung12 <gkr097@naver.com>
2024-09-09 13:40:52 +0900

    test: mock data 추가

---
+++ src/app/components/BottomFloatingBar/Mock.ts
@@ -0,0 +1,43 @@
+/* user mock data */
+export const userData = {
+  name: 'test name',
+  id: 1234,
+};
+
+/* 모임 상세 조회 mock data */
+export const groupData = {
+  id: 0,
+  registrationEnd: '2024-09-15T23:59:59Z',
+  participantCount: 5,
+  capacity: 10,
+  createdBy: 1234,
+  canceledAt: null,
+};
+
+export const participantsData = [
+  {
+    User: {
+      id: 1234,
+    },
+  },
+  {
+    User: {
+      id: 1534,
+    },
+  },
+  {
+    User: {
+      id: 1344,
+    },
+  },
+  {
+    User: {
+      id: 1654,
+    },
+  },
+  {
+    User: {
+      id: 1634,
+    },
+  },
+];
