90200c99cf55c03d4baee1be48bd71d7d604b6cf
hakyoung12 <gkr097@naver.com>
2024-09-10 16:15:13 +0900

    [KAN-29] style: 로그인/회원가입 반응형 레이아웃 적용

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -8,20 +8,21 @@ const Layout = ({
 }>) => {
   return (
     <div className='min-h-dvh w-full bg-var-gray-100'>
-      <div className='flex max-w-[1200px] flex-col items-center justify-between gap-32 pt-40 md:mx-24 lg:mx-auto lg:flex-row lg:pt-40'>
-        <div className='flex flex-col'>
-          <div className='text-center'>
-            <h1 className='text-20 font-semibold md:text-24'>
-              Welcome to 같이 달램!
-            </h1>
-            <p className='text-14 font-medium md:text-16'>
-              바쁜 일상 속 잠깐의 휴식, <br />
-              이제는 같이 달램과 함께 해보세요
-            </p>
-          </div>
-          <ImageLogin className='w-[588px]' />
+      <div className='flex flex-col items-center justify-between gap-32 py-40 md:mx-24 lg:mx-auto lg:max-w-[1200px] lg:flex-row lg:gap-100 lg:px-24 lg:pt-40'>
+        {/* 이미지 영역 */}
+        <div className='text-center'>
+          <h1 className='text-20 font-semibold md:text-24'>
+            Welcome to 같이 달램!
+          </h1>
+          <p className='text-14 font-medium md:text-16'>
+            바쁜 일상 속 잠깐의 휴식, <br />
+            이제는 같이 달램과 함께 해보세요
+          </p>
+          <ImageLogin className='w-[290px] md:w-[407px] lg:w-[588px]' />
         </div>
-        <div className='rounded-[24px] bg-var-white px-[54px] py-32'>
+
+        {/* form 영역 */}
+        <div className='w-[343px] rounded-[24px] bg-var-white px-16 py-32 md:w-[510px] md:px-[54px]'>
           {children}
           <Footer />
         </div>
--- src/app/components/AuthForm/SignInForm.tsx
+++ src/app/components/AuthForm/SignInForm.tsx
@@ -13,7 +13,7 @@ interface SigninnData {
 const SignInForm = () => {
   return (
     <form className='rounded-[24px] bg-var-white'>
-      <div className='flex w-full flex-col gap-24'>
+      <div className='flex flex-col gap-24'>
         <h1 className='pb-8 text-center text-24 font-semibold'>로그인</h1>
         <div className='flex flex-col gap-4'>
           <label className='text-14 font-semibold'>아이디</label>
