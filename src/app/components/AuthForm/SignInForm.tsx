5945bd37be0c62214f6c53fd8050b7e4817e1a67
hakyoung12 <gkr097@naver.com>
2024-09-10 15:05:22 +0900

    [KAN-29] rename: 로그인폼 이름변경

--- src/app/components/AuthForm/SignInForm.tsx
+++ src/app/components/AuthForm/SignInForm.tsx
@@ -12,7 +12,7 @@ interface LoginData {
 
 const LoginForm = () => {
   return (
-    <form className='max-w-[510px] rounded-[24px] bg-var-white px-[54px] py-32'>
+    <form className='rounded-[24px] bg-var-white'>
       <div className='flex w-full flex-col gap-24'>
         <h1 className='pb-8 text-center text-24 font-semibold'>로그인</h1>
         <div className='flex flex-col gap-4'>
@@ -32,12 +32,6 @@ const LoginForm = () => {
           variant='default'
           disabled={false}
         />
-        <p className='flex items-center justify-center text-[15px] font-medium'>
-          같이 달램이 처음이신가요?
-          <span className='text-var-orange-600 underline'>
-            <Link href='/signup'>회원가입</Link>
-          </span>
-        </p>
       </div>
     </form>
   );
