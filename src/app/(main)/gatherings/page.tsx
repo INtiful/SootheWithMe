9b4b6086a128938af647ad6076a159fe7c50db8a
Homin <coc1381@naver.com>
2024-09-10 11:32:57 +0900

    [KAN-33] style: gatherings 메인 페이지 ui 작성

---
+++ src/app/(main)/gatherings/page.tsx
@@ -0,0 +1,114 @@
+'use client';
+
+import { useState } from 'react';
+
+import { HeadClass } from '@/public/images';
+import Tab from '@/app/components/Tab/Tab';
+import Button from '@/app/components/Button/Button';
+import Chip from '@/app/components/Chip/Chip';
+import FilterList from '@/app/components/Filter/FilterList';
+import FilterSort from '@/app/components/Filter/FilterSort';
+import CardList from '@/app/components/CardList/CardList';
+
+const tomorrow = new Date();
+tomorrow.setDate(tomorrow.getDate() + 1);
+
+const registrationEnd = new Date(tomorrow);
+registrationEnd.setDate(tomorrow.getDate() - 1);
+
+const mockData = {
+  teamId: 101,
+  id: 1,
+  type: 'Fitness',
+  name: 'Fitness Gathering',
+  dateTime: tomorrow.toISOString(),
+  registrationEnd: registrationEnd.toISOString(),
+  location: 'Seoul, Korea',
+  participantCount: 8,
+  capacity: 15,
+  image: '/images/mock-image.png',
+  createdBy: 1001,
+  canceledAt: registrationEnd.toISOString(),
+};
+
+const GatheringsPage = () => {
+  const [activeTab, setActiveTab] = useState<'workation' | 'dalaemfit'>(
+    'dalaemfit',
+  );
+
+  const handleTabClick = (type: 'workation' | 'dalaemfit') => {
+    setActiveTab(type);
+  };
+
+  return (
+    <div className='mx-auto max-w-[1200px]'>
+      <div className='min-h-screen bg-var-gray-50 px-16 pt-24 md:px-24 md:pt-40 lg:px-100'>
+        {/* 헤더 */}
+        <div className='flex space-x-16'>
+          <div>
+            <HeadClass className='h-72 w-72' />
+          </div>
+          <div className='space-y-8'>
+            <div className='text-14 font-medium'>함께 할 사람이 없나요?</div>
+            <div className='text-[18px] font-semibold md:text-24'>
+              지금 모임에 참여해보세요
+            </div>
+          </div>
+        </div>
+
+        {/* 구분선 넣기 위한 div */}
+        <div className='divide-y'>
+          <div>
+            {/* 탭 */}
+            <div className='mt-32 flex justify-between'>
+              <div className='flex space-x-12'>
+                <Tab
+                  type='dalaemfit'
+                  isActive={activeTab === 'dalaemfit'}
+                  onClick={() => handleTabClick('dalaemfit')}
+                />
+                <Tab
+                  type='workation'
+                  isActive={activeTab === 'workation'}
+                  onClick={() => handleTabClick('workation')}
+                />
+              </div>
+              <div className='w-[100px] md:w-[115px]'>
+                <Button name='모임 만들기' variant='default' />
+              </div>
+            </div>
+
+            {/* 버튼 탭 */}
+            <div className='mt-8 space-x-8 py-16'>
+              <Chip state='active'>전체</Chip>
+              <Chip state='default'>오피스 스트레칭</Chip>
+              <Chip state='default'>마인드풀니스</Chip>
+            </div>
+          </div>
+
+          {/* 드롭다운 */}
+          <div className='flex justify-between pt-16'>
+            <div className='flex space-x-8'>
+              <FilterList state='default'>지역 전체</FilterList>
+              <FilterList state='default'>날짜 전체</FilterList>
+            </div>
+            <div>
+              <FilterSort state='default'>마감임박</FilterSort>
+            </div>
+          </div>
+        </div>
+
+        {/* 카드 리스트 */}
+        <div className='mt-24 space-y-24'>
+          <CardList data={mockData} />
+          <CardList data={mockData} />
+          <CardList data={mockData} />
+          <CardList data={mockData} />
+          <CardList data={mockData} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default GatheringsPage;
