086773df21fa7785d4905d2859d7fef244cdee47
Homin <coc1381@naver.com>
2024-09-10 11:40:27 +0900

    [KAN-33] style: 모임이 없는 경우 메시지

--- src/app/(main)/gatherings/page.tsx
+++ src/app/(main)/gatherings/page.tsx
@@ -100,6 +100,16 @@ const GatheringsPage = () => {
 
         {/* 카드 리스트 */}
         <div className='mt-24 space-y-24'>
+          {/* 모임이 없는 경우 */}
+          {/* <div className='flex h-[400px] items-center justify-center'>
+            <p className='text-center text-14 font-medium text-var-gray-500'>
+              아직 모임이 없어요.
+              <br />
+              지금 바로 모임을 만들어보세요.
+            </p>
+          </div> */}
+
+          {/* 모임이 있는 경우 */}
           <CardList data={mockData} />
           <CardList data={mockData} />
           <CardList data={mockData} />
