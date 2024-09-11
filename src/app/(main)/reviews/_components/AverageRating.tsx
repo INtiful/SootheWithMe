85c7a2af6def45f8656c7feae8590bab9d092924
chaeney <chaekey@gmail.com>
2024-09-11 15:44:43 +0900

    [KAN-32] feat: 모든 리뷰 페이지 UI 구현

---
+++ src/app/(main)/reviews/_components/AverageRating.tsx
@@ -0,0 +1,57 @@
+import { IconHeart } from '@/public/icons';
+import { RATING_DATA as data } from '../mockData';
+
+const calculateAverageRating = () => {
+  // 각 별점 값에 별점 수를 곱한 총합 계산
+  const totalScore =
+    data.oneStar * 1 +
+    data.twoStars * 2 +
+    data.threeStars * 3 +
+    data.fourStars * 4 +
+    data.fiveStars * 5;
+
+  // 전체 별점 개수 합산
+  const totalCount =
+    data.oneStar +
+    data.twoStars +
+    data.threeStars +
+    data.fourStars +
+    data.fiveStars;
+
+  // 총합을 별점 개수로 나눠 평균 계산
+  const average = totalCount === 0 ? 0 : totalScore / totalCount;
+
+  return average.toFixed(1); // 소수점 둘째 자리까지 표시
+};
+
+const AverageRating = () => {
+  const rating = calculateAverageRating();
+  const width = parseFloat(rating) * 20;
+
+  return (
+    <div>
+      <p className='text-center text-24 font-semibold text-var-gray-400'>
+        <span className='mr-2 text-var-gray-900'>{rating}</span>/ 5
+      </p>
+      <div className='relative flex gap-[2px] text-var-gray-200'>
+        <IconHeart className='size-24 shrink-0' />
+        <IconHeart className='size-24 shrink-0' />
+        <IconHeart className='size-24 shrink-0' />
+        <IconHeart className='size-24 shrink-0' />
+        <IconHeart className='size-24 shrink-0' />
+        <div
+          className='absolute left-0 top-0 flex gap-[2px] overflow-hidden text-var-orange-600'
+          style={{ width: `${width}%` }}
+        >
+          <IconHeart className='size-24 shrink-0' />
+          <IconHeart className='size-24 shrink-0' />
+          <IconHeart className='size-24 shrink-0' />
+          <IconHeart className='size-24 shrink-0' />
+          <IconHeart className='size-24 shrink-0' />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AverageRating;
---
+++ src/app/(main)/reviews/page.tsx
@@ -0,0 +1,138 @@
+'use client';
+import { HeadReview } from '@/public/images';
+import { useState } from 'react';
+import { OPTIONS, RATING_DATA, SORT_OPTIONS, tempData } from './mockData';
+import Scores from './_components/Scores';
+import Tab from '@/app/components/Tab/Tab';
+import Chip from '@/app/components/Chip/Chip';
+import Review from '@/app/components/Review/Review';
+import AverageRating from './_components/AverageRating';
+import FilterList from '@/app/components/Filter/FilterList';
+import FilterSort from '@/app/components/Filter/FilterSort';
+
+const ReviewsPage = () => {
+  const [activeTab, setActiveTab] = useState<'dalaemfit' | 'workation'>(
+    'dalaemfit',
+  );
+
+  const [activeChip, setActiveChip] = useState<
+    'all' | 'officeStretching' | 'mindfulness'
+  >('all');
+
+  const handleClickTab = (type: 'dalaemfit' | 'workation') => {
+    setActiveTab(type);
+    if (type === 'workation') {
+      setActiveChip('all');
+    }
+  };
+
+  const handleClickChips = (
+    type: 'all' | 'officeStretching' | 'mindfulness',
+  ) => {
+    setActiveChip(type);
+  };
+
+  return (
+    <main className='mx-auto flex h-full max-w-1200 flex-col bg-var-gray-50 px-16 pt-24 md:px-24 md:pt-40 lg:px-100'>
+      {/* head */}
+      <div className='flex items-center gap-16'>
+        <HeadReview className='h-72 w-72' />
+        <div>
+          <h2 className='mb-8 text-18 font-semibold text-var-gray-900'>
+            모든 리뷰
+          </h2>
+          <p className='text-14 font-medium text-var-gray-700'>
+            같이달램을 이용한 분들은 이렇게 느꼈어요 🙌
+          </p>
+        </div>
+      </div>
+
+      <section className='mt-24 flex grow flex-col md:mt-32'>
+        {/* tab */}
+        <div className='mb-12 flex items-center'>
+          <Tab
+            type='dalaemfit'
+            isActive={activeTab === 'dalaemfit'}
+            onClick={() => handleClickTab('dalaemfit')}
+          />
+          <Tab
+            type='workation'
+            isActive={activeTab === 'workation'}
+            onClick={() => handleClickTab('workation')}
+          />
+        </div>
+        {/* filter */}
+        <div className='flex flex-col gap-16'>
+          <div
+            className={`flex items-center gap-8 ${activeTab === 'workation' ? 'hidden' : ''}`}
+          >
+            <button type='button' onClick={() => handleClickChips('all')}>
+              <Chip state={activeChip === 'all' ? 'active' : 'default'}>
+                전체
+              </Chip>
+            </button>
+            <button
+              type='button'
+              onClick={() => handleClickChips('officeStretching')}
+            >
+              <Chip
+                state={activeChip === 'officeStretching' ? 'active' : 'default'}
+              >
+                오피스 스트레칭
+              </Chip>
+            </button>
+            <button
+              type='button'
+              onClick={() => handleClickChips('mindfulness')}
+            >
+              <Chip state={activeChip === 'mindfulness' ? 'active' : 'default'}>
+                마인드풀니스
+              </Chip>
+            </button>
+          </div>
+          <div className='w-full border-y border-var-gray-200' />
+          {/* 별점칸 */}
+          <div className='flex items-center justify-center border-y-2 border-var-gray-200 bg-white p-32'>
+            <div className='flex w-full items-center justify-between gap-24 md:w-auto md:gap-120 lg:w-612'>
+              <AverageRating />
+              <Scores />
+            </div>
+          </div>
+        </div>
+        {/* 데이터 */}
+        <div className='mt-24 border-t-2 border-t-var-gray-900 bg-white p-24'>
+          <div className='mb-24 flex items-center justify-between'>
+            <div className='flex gap-8'>
+              <FilterList state='default' options={OPTIONS}>
+                지역 선택
+              </FilterList>
+              <FilterList state='default'>날짜 선택</FilterList>
+            </div>
+            <div>
+              <FilterSort state='default' options={SORT_OPTIONS}>
+                최신순
+              </FilterSort>
+            </div>
+          </div>
+
+          <div className='space-y-12'>
+            {tempData.map((item, index) => (
+              <Review
+                image_source={item.image_source}
+                rating={item.rating}
+                description={item.description}
+                place={item.place}
+                location={item.location}
+                user_name={item.user_name}
+                date={item.date}
+                key={index}
+              />
+            ))}
+          </div>
+        </div>
+      </section>
+    </main>
+  );
+};
+
+export default ReviewsPage;
