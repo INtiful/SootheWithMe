9f55f45a5a6552acc42b8d0178445171917b2609
hakyoung12 <gkr097@naver.com>
2024-09-11 11:12:22 +0900

    [KAN-29] refactor: form 유효성 검사 zod 적용

--- src/app/components/AuthForm/SignInForm.tsx
+++ src/app/components/AuthForm/SignInForm.tsx
@@ -1,9 +1,10 @@
 'use client';
 
-import { Controller, useForm } from 'react-hook-form';
+import { useForm } from 'react-hook-form';
 import Button from '../Button/Button';
 import Input from '../Input/Input';
-import FORM_OPTIONS from './FormOptions';
+import { errorMessageStyle, signinSchema } from './FormOptions';
+import { zodResolver } from '@hookform/resolvers/zod';
 
 interface SignInData {
   email: string;
@@ -13,10 +14,12 @@ interface SignInData {
 const SignInForm = () => {
   const {
     control,
+    register,
     handleSubmit,
     formState: { errors, isValid },
   } = useForm<SignInData>({
     mode: 'onChange',
+    resolver: zodResolver(signinSchema),
     defaultValues: {
       email: '',
       password: '',
@@ -36,46 +39,27 @@ const SignInForm = () => {
         <h1 className='pb-8 text-center text-24 font-semibold'>로그인</h1>
         <div className='flex flex-col gap-4'>
           <label className='text-14 font-semibold'>아이디</label>
-          <Controller
-            control={control}
-            name={FORM_OPTIONS.email.name}
-            rules={FORM_OPTIONS.email.rules}
-            defaultValue=''
-            render={({ field }) => (
-              <Input
-                placeholder='이메일을 입려해주세요.'
-                className='h-44'
-                hasError={!!errors.email}
-                {...field}
-              />
-            )}
+
+          <Input
+            placeholder='이메일을 입려해주세요.'
+            className='h-44'
+            hasError={!!errors.email}
+            {...register('email')}
           />
           {errors.email && (
-            <div className={FORM_OPTIONS.errorMessageStyle}>
-              {errors.email.message}
-            </div>
+            <div className={errorMessageStyle}>{errors.email.message}</div>
           )}
         </div>
         <div className='flex flex-col gap-4'>
           <label className='text-14 font-semibold'>비밀번호</label>
-          <Controller
-            control={control}
-            name={FORM_OPTIONS.loginPassword.name}
-            rules={FORM_OPTIONS.loginPassword.rules}
-            defaultValue=''
-            render={({ field }) => (
-              <Input
-                placeholder='비밀번호를 입력해주세요.'
-                className='h-44'
-                hasError={!!errors.password}
-                {...field}
-              />
-            )}
+          <Input
+            placeholder='비밀번호를 입력해주세요.'
+            className='h-44'
+            hasError={!!errors.password}
+            {...register('password')}
           />
           {errors.password && (
-            <div className={FORM_OPTIONS.errorMessageStyle}>
-              {errors.password.message}
-            </div>
+            <div className={errorMessageStyle}>{errors.password.message}</div>
           )}
         </div>
         <Button
