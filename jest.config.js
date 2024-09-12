c4f95b663a8c9cda92b09fe295563484ef907f50
Homin <coc1381@naver.com>
2024-09-12 14:46:59 +0900

    [KAN-58] fix: jest setting 변경

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,18 @@
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'jsdom', // React 환경에서 테스트할 경우 jsdom 사용
-  transform: {
-    '^.+\\.tsx?$': 'ts-jest', // TypeScript 파일을 ts-jest로 변환
-  },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+const nextJest = require('next/jest');
+
+/** @type {import('jest').Config} */
+const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  dir: './',
+});
+
+// Add any custom config to be passed to Jest
+const config = {
+  coverageProvider: 'v8',
+  testEnvironment: 'jsdom',
+  // Add more setup options before each test is run
+  // setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
 };
+
+// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+module.exports = createJestConfig(config);
