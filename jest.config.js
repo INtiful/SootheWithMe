91c849ded7c1d6f4150cf8db3e51eda301bb511e
Homin <coc1381@naver.com>
2024-09-12 14:34:10 +0900

    jest 실행 환경 test

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  testEnvironment: 'jest-environment-jsdom', // Jest가 테스트를 실행할 환경을 설정
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'], // Jest가 테스트 실행 전에 실행할 파일을 지정
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom', // React 환경에서 테스트할 경우 jsdom 사용
   transform: {
-    '^.+\\.tsx?$': 'ts-jest',
-  }, // Jest가 TypeScript 파일을 처리할 때 사용할 변환기를 지정
-  testMatch: ['<rootDir>/src/**/*.(spec|test).[jt]s?(x)'], // 테스트 파일의 위치와 이름 패턴을 지정
+    '^.+\\.tsx?$': 'ts-jest', // TypeScript 파일을 ts-jest로 변환
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
 };
