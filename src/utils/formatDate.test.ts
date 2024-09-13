d2cb7022f2f27e2908003bdba90aba9ce8adb382
chaeney <chaekey@gmail.com>
2024-09-13 15:24:45 +0900

    [KAN-49] test: formatDate 유틸 함수 Bad Case 테스트 코드 추가

--- src/utils/formatDate.test.ts
+++ src/utils/formatDate.test.ts
@@ -7,10 +7,15 @@ import {
 } from './formatDate';
 
 const DATE_STRING = '2024-09-11T19:30:00';
+const NOT_DATE_STRING = 'NOT_DATE_STRING';
 
 describe('formatDate', () => {
   it('should format as `m월 dd일`', () => {
     expect(formatDate(DATE_STRING)).toBe('9월 11일');
+  });
+
+  it('should return `NaN월 NaN일` for an invalid date string', () => {
+    expect(formatDate(NOT_DATE_STRING)).toBe('NaN월 NaN일');
   });
 });
 
@@ -18,17 +23,29 @@ describe('formatTime', () => {
   it('should format as `HH시 mm분`', () => {
     expect(formatTime(DATE_STRING)).toBe('19시 30분');
   });
+
+  it('should return `NaN시 NaN분`', () => {
+    expect(formatTime(NOT_DATE_STRING)).toBe('NaN시 NaN분');
+  });
 });
 
 describe('formatTimeHours', () => {
   it('should format as `HH`', () => {
     expect(formatTimeHours(DATE_STRING)).toBe('19');
   });
+
+  it('should return `NaN`', () => {
+    expect(formatTimeHours(NOT_DATE_STRING)).toBe('NaN');
+  });
 });
 
 describe('formatTimeColon', () => {
   it('should format as `HH:mm`', () => {
     expect(formatTimeColon(DATE_STRING)).toBe('19:30');
+  });
+
+  it('should return `NaN:NaN`', () => {
+    expect(formatTimeColon(NOT_DATE_STRING)).toBe('NaN:NaN');
   });
 });
 
@@ -40,5 +57,9 @@ describe('isSameDate', () => {
 
   it('should return false if the date is not the same as today', () => {
     expect(isSameDate(DATE_STRING)).toBe(false);
+  });
+
+  it('should return false', () => {
+    expect(isSameDate(NOT_DATE_STRING)).toBe(false);
   });
 });
