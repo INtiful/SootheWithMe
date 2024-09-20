54caf33d3f921ee0a86c830a0a856806040143ad
Matthew Song <songww1997@naver.com>
2024-09-20 11:10:33 +0900

    [KAN-27] feat: 마이페이지 레이아웃 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,3 @@
-'use client';
-
-import { useState } from 'react';
-import ReviewModal from './components/Modal/ReviewModal';
-
 export default function Home() {
-  const [reviewComment, setReviewComment] = useState('');
-  return (
-    <main className='flex h-screen items-center justify-center'>
-      <ReviewModal
-        reviewComment={reviewComment}
-        onChangeReviewComment={(e: any) => setReviewComment(e.target.value)}
-      />
-    </main>
-  );
+  return <main></main>;
 }
