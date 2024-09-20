c3a771297cd5c989889b5a236e415b49b3c13a4a
Matthew Song <songww1997@naver.com>
2024-09-20 14:04:10 +0900

    [KAN-27] feat: 필터 버튼 구현

--- src/app/(main)/mypage/review/page.tsx
+++ src/app/(main)/mypage/review/page.tsx
@@ -1,14 +1,20 @@
 'use client';
 
 import Card from '@/app/components/Card/Card';
-import { DATA_LIST } from '../created/mockData';
-import { useState } from 'react';
-import usePreventScroll from '@/hooks/usePreventScroll';
+import Chip from '@/app/components/Chip/Chip';
 import ReviewModal from '@/app/components/Modal/ReviewModal';
+import usePreventScroll from '@/hooks/usePreventScroll';
+import { useState } from 'react';
+import { DATA_LIST } from '../created/mockData';
 
 const Page = () => {
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
   const [reviewComment, setReviewComment] = useState<string>('');
+
+  const filterTypeList = ['작성 가능한 리뷰', '작성한 리뷰'] as const;
+  const [filterType, setFilterType] = useState<(typeof filterTypeList)[number]>(
+    filterTypeList[0],
+  );
 
   const handleChangeReviewComment = (
     e: React.ChangeEvent<HTMLTextAreaElement>,
@@ -16,11 +22,37 @@ const Page = () => {
     setReviewComment(e.target.value);
   };
 
+  const handleChangeFilterType = (type: (typeof filterTypeList)[number]) => {
+    setFilterType(type);
+  };
+
   usePreventScroll(isModalOpen);
 
   return (
     <>
-      <div className='grow'>
+      <div className='grow pt-16'>
+        {/* chips */}
+        <div className='flex items-center gap-8'>
+          <button
+            onClick={() => handleChangeFilterType('작성 가능한 리뷰')}
+            className='transition-all duration-200 ease-in-out'
+          >
+            <Chip
+              state={filterType === '작성 가능한 리뷰' ? 'active' : 'default'}
+            >
+              작성 가능한 리뷰
+            </Chip>
+          </button>
+          <button
+            onClick={() => handleChangeFilterType('작성한 리뷰')}
+            className='transition-all duration-200 ease-in-out'
+          >
+            <Chip state={filterType === '작성한 리뷰' ? 'active' : 'default'}>
+              작성한 리뷰
+            </Chip>
+          </button>
+        </div>
+        {/* cards */}
         {DATA_LIST.map((data) => (
           <Card
             key={data.id}
