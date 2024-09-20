376dc3f21cd718f54f859ee738e1ee59948ee3f5
Matthew Song <songww1997@naver.com>
2024-09-20 15:52:26 +0900

    [KAN-27] refactor: 상수화와 코드 개선

--- src/app/(main)/mypage/review/page.tsx
+++ src/app/(main)/mypage/review/page.tsx
@@ -2,21 +2,24 @@
 
 import Card from '@/app/components/Card/Card';
 import ReviewModal from '@/app/components/Modal/ReviewModal';
+import Review from '@/app/components/Review/Review';
+import { MYPAGE_REVIEW_TABS } from '@/constants/common';
 import usePreventScroll from '@/hooks/usePreventScroll';
 import { ChangeEvent, useState } from 'react';
-import { DATA_LIST } from '../created/mockData';
 import ReviewFilterButtons from '../_component/ReviewFilterButtons';
+import { DATA_LIST } from '../created/mockData';
+import { MYPAGE_REVIEW_MOCK_DATA } from './mock';
 
 const Page = () => {
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
   const [reviewComment, setReviewComment] = useState<string>('');
-  const [filterType, setFilterType] = useState<string>('');
+  const [filterType, setFilterType] = useState<string>(MYPAGE_REVIEW_TABS.ALL);
 
   const filteredData = DATA_LIST.filter((data) => {
     switch (filterType) {
-      case '작성 가능한 리뷰':
+      case MYPAGE_REVIEW_TABS.WRITABLE: // 작성 가능한 리뷰
         return data.isCompleted && !data.isReviewed;
-      case '작성한 리뷰':
+      case MYPAGE_REVIEW_TABS.WRITTEN: // 작성한 리뷰
         return data.isCompleted && data.isReviewed;
       default:
         return true;
@@ -43,13 +46,17 @@ const Page = () => {
           setFilterType={setFilterType}
         />
         {/* cards */}
+        {/* 전체 혹은 작성 가능한 리뷰 */}
         {filteredData.length === 0 ? (
           <div className='flex h-full items-center justify-center'>
             <p className='text-center text-14 font-medium text-var-gray-500'>
-              리뷰할 모임이 없습니다.
+              아직 작성 가능한 리뷰가 없어요
             </p>
           </div>
         ) : (
+          [MYPAGE_REVIEW_TABS.ALL, MYPAGE_REVIEW_TABS.WRITABLE].includes(
+            filterType,
+          ) &&
           filteredData.map((data) => (
             <Card
               key={data.id}
@@ -60,6 +67,20 @@ const Page = () => {
             />
           ))
         )}
+        {/* 작성한 리뷰 */}
+        <div className='my-24 flex flex-col gap-24'>
+          {filterType === MYPAGE_REVIEW_TABS.WRITTEN &&
+            MYPAGE_REVIEW_MOCK_DATA.map((review) => (
+              <Review
+                key={review.id}
+                rating={review.score}
+                image_source={review.Gathering.image}
+                description={review.comment}
+                user_name={review.User.name}
+                date={review.createdAt}
+              />
+            ))}
+        </div>
       </div>
       {isModalOpen && (
         <ReviewModal
--- src/constants/common.ts
+++ src/constants/common.ts
@@ -1,5 +1,12 @@
-// 모임 시간대
+// 모임 만들기에서 가능한 시간대
 export const GATHERING_TIMES = {
   MORNING: ['09:00', '10:00', '11:00'],
   AFTERNOON: ['12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00'],
+};
+
+// 마이페이지 리뷰 탭
+export const MYPAGE_REVIEW_TABS = {
+  ALL: '',
+  WRITABLE: '작성 가능한 리뷰',
+  WRITTEN: '작성한 리뷰',
 };
