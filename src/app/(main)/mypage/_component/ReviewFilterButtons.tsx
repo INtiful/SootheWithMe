86c879483532e15cfd10bf3b0a44990033daea75
Matthew Song <songww1997@naver.com>
2024-09-20 14:16:37 +0900

    [KAN-27] refactor: 필터 버튼 로직 수정

--- src/app/(main)/mypage/_component/ReviewFilterButtons.tsx
+++ src/app/(main)/mypage/_component/ReviewFilterButtons.tsx
@@ -3,32 +3,41 @@
 import Chip from '@/app/components/Chip/Chip';
 import { useState } from 'react';
 
-const ReviewFilterButtons = () => {
+interface ReviewFilterButtonsProps {
+  filterType: string;
+  setFilterType: (type: string) => void;
+}
+
+const ReviewFilterButtons = ({
+  filterType,
+  setFilterType,
+}: ReviewFilterButtonsProps) => {
   const filterTypeList = ['작성 가능한 리뷰', '작성한 리뷰'] as const;
-  const [filterType, setFilterType] = useState<(typeof filterTypeList)[number]>(
-    filterTypeList[0],
-  );
 
   const handleChangeFilterType = (type: (typeof filterTypeList)[number]) => {
-    setFilterType(type);
+    if (filterType === type) {
+      setFilterType('');
+    } else {
+      setFilterType(type);
+    }
   };
 
   return (
     <div className='flex items-center gap-8'>
       <button
-        onClick={() => handleChangeFilterType('작성 가능한 리뷰')}
+        onClick={() => handleChangeFilterType(filterTypeList[0])}
         className='transition-all duration-200 ease-in-out'
       >
-        <Chip state={filterType === '작성 가능한 리뷰' ? 'active' : 'default'}>
-          작성 가능한 리뷰
+        <Chip state={filterType === filterTypeList[0] ? 'active' : 'default'}>
+          {filterTypeList[0]}
         </Chip>
       </button>
       <button
-        onClick={() => handleChangeFilterType('작성한 리뷰')}
+        onClick={() => handleChangeFilterType(filterTypeList[1])}
         className='transition-all duration-200 ease-in-out'
       >
-        <Chip state={filterType === '작성한 리뷰' ? 'active' : 'default'}>
-          작성한 리뷰
+        <Chip state={filterType === filterTypeList[1] ? 'active' : 'default'}>
+          {filterTypeList[1]}
         </Chip>
       </button>
     </div>
--- src/app/(main)/mypage/review/page.tsx
+++ src/app/(main)/mypage/review/page.tsx
@@ -3,17 +3,16 @@
 import Card from '@/app/components/Card/Card';
 import ReviewModal from '@/app/components/Modal/ReviewModal';
 import usePreventScroll from '@/hooks/usePreventScroll';
-import { useState } from 'react';
+import { ChangeEvent, useState } from 'react';
 import { DATA_LIST } from '../created/mockData';
 import ReviewFilterButtons from '../_component/ReviewFilterButtons';
 
 const Page = () => {
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
   const [reviewComment, setReviewComment] = useState<string>('');
+  const [filterType, setFilterType] = useState<string>('');
 
-  const handleChangeReviewComment = (
-    e: React.ChangeEvent<HTMLTextAreaElement>,
-  ) => {
+  const handleChangeReviewComment = (e: ChangeEvent<HTMLTextAreaElement>) => {
     setReviewComment(e.target.value);
   };
 
@@ -23,7 +22,10 @@ const Page = () => {
     <>
       <div className='grow pt-16'>
         {/* chips */}
-        <ReviewFilterButtons />
+        <ReviewFilterButtons
+          filterType={filterType}
+          setFilterType={setFilterType}
+        />
         {/* cards */}
         {DATA_LIST.map((data) => (
           <Card
