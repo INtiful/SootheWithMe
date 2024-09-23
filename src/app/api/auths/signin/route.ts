e38c998ebfed066c55da38489bfe7f2fe373162e
hakyoung12 <gkr097@naver.com>
2024-09-23 14:33:48 +0900

    [KAN-70] chore: route 함수 이름변경

--- src/app/api/auths/signin/route.ts
+++ src/app/api/auths/signin/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
 
-export const Post = async (req: Request) => {
+export const POST = async (req: Request) => {
   const data = await req.json();
 
   return NextResponse.json({ message: '로그인 성공' }, { status: 201 });
--- src/app/api/auths/signup/route.ts
+++ src/app/api/auths/signup/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
 
-export const Post = async (req: Request) => {
+export const POST = async (req: Request) => {
   const data = await req.json();
 
   return NextResponse.json({ message: '사용자 생성 성공' }, { status: 201 });
