748f8bba6163f10d4058fc4612f956d816572b72
chaeney <chaekey@gmail.com>
2024-09-23 09:22:51 +0900

    [KAN-65] chore: 내가 만든 모임 페이지 merge를 위한 코드 수정

--- src/app/(main)/mypage/created/page.tsx
+++ src/app/(main)/mypage/created/page.tsx
@@ -11,26 +11,17 @@ const CreatedPage = () => {
   };
 
   return (
-    <section className='flex w-full grow flex-col border-t-2 border-var-gray-900 bg-white px-16 py-24 md:px-24'>
-      <Tab />
-
-      <div className='grow'>
-        {DATA_LIST.map((data) => (
-          <Card
-            key={data.id}
-            handleSaveDiscard={handleSaveDiscard}
-            data={DATA_LIST[0]}
-          >
-            <Card.Info />
-          </Card>
-        ))}
-      </div>
-
-      {/* 내가 만든 모임이 없을 경우 */}
-      {/* <div className='flex grow items-center justify-center text-14 font-medium text-var-gray-500'>
-        아직 만든 모임이 없어요
-      </div> */}
-    </section>
+    <div className='grow'>
+      {DATA_LIST.map((data) => (
+        <Card key={data.id} handleSaveDiscard={handleSaveDiscard} data={data}>
+          <Card.Info />
+        </Card>
+      ))}
+    </div>
+    // 내가 만든 모임이 없을 경우
+    // <div className='flex grow items-center justify-center text-14 font-medium text-var-gray-500'>
+    //   아직 만든 모임이 없어요
+    // </div>
   );
 };
 
