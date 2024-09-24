92a84e858cc4a1cf83ddb9d41ff13d180f977e74
hakyoung12 <gkr097@naver.com>
2024-09-24 17:04:11 +0900

    [KAN-71] feat: Card 컴포넌트 이미지 없을 때 렌더링할 이미지 추가

--- src/app/api/gatherings/service/getMyGathergins.ts
+++ src/app/api/gatherings/service/getMyGathergins.ts
@@ -11,11 +11,7 @@ const getMyGathergins = async (
   );
 
   if (!response.ok) {
-<<<<<<< HEAD
     throw new Error('나의 모임을 불러오는데 실패했습니다.');
-=======
-    throw new Error('Network response was not ok');
->>>>>>> 83b8fdf ([KAN-71] feat: 나의 모임 api 연결)
   }
   return response.json();
 };
