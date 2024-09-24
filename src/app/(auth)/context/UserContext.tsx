ed441a4bc6fd6310e14698d79afd0a730411c300
hakyoung12 <gkr097@naver.com>
2024-09-24 09:45:46 +0900

    [KAN-70] chore: state 이름 변경

--- src/app/(auth)/context/UserContext.tsx
+++ src/app/(auth)/context/UserContext.tsx
@@ -11,16 +11,16 @@ import React, {
 
 interface UserContextType {
   user: UserData | null;
-  loading: boolean;
-  error: string | null;
+  isloading: boolean;
+  errorMsg: string | null;
 }
 
 const UserContext = createContext<UserContextType | undefined>(undefined);
 
 export const UserProvider: FC<{ children: ReactNode }> = ({ children }) => {
   const [user, setUser] = useState<UserData | null>(null);
-  const [loading, setLoading] = useState<boolean>(false);
-  const [error, setError] = useState<string | null>(null);
+  const [isloading, setIsLoading] = useState<boolean>(false);
+  const [errorMsg, setErrorMsg] = useState<string | null>(null);
 
   useEffect(() => {
     const fetchUserData = async () => {
@@ -30,8 +30,8 @@ export const UserProvider: FC<{ children: ReactNode }> = ({ children }) => {
         return;
       }
 
-      setLoading(true);
-      setError(null);
+      setIsLoading(true);
+      setErrorMsg(null);
       try {
         const userResponse = await fetch(
           `${process.env.NEXT_PUBLIC_API_BASE_URL}/auths/user`,
@@ -49,9 +49,9 @@ export const UserProvider: FC<{ children: ReactNode }> = ({ children }) => {
           throw new Error(errorData.message);
         }
       } catch (err: any) {
-        setError(err.message);
+        setErrorMsg(err.message);
       } finally {
-        setLoading(false);
+        setIsLoading(false);
       }
     };
 
@@ -60,7 +60,7 @@ export const UserProvider: FC<{ children: ReactNode }> = ({ children }) => {
   }, []); // 컴포넌트 마운트 시 한 번만 호출
 
   return (
-    <UserContext.Provider value={{ user, loading, error }}>
+    <UserContext.Provider value={{ user, isloading, errorMsg }}>
       {children}
     </UserContext.Provider>
   );
