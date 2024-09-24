e7f066edc2b660d83447ef8ab06588614feaeb0f
hakyoung12 <gkr097@naver.com>
2024-09-24 13:22:53 +0900

    [KAN-70] rename: 로그인 회원가입 로직 파일 이돌

--- src/app/(auth)/signin/_component/SignInForm.tsx
+++ src/app/(auth)/signin/_component/SignInForm.tsx
@@ -7,7 +7,7 @@ import { signinSchema } from '../../_component/FormOptions';
 import { zodResolver } from '@hookform/resolvers/zod';
 import FormField from '../../_component/FormField';
 import { SignInData } from '@/types/client.type';
-import { submitSignInData } from '@/actions/auth/postSignIn';
+import { submitSignInData } from '@/app/api/auths/service/postSignIn';
 
 const SignInForm = () => {
   const router = useRouter();
