4fd4a624a910699bf6ea4dfbadc3eba165cc60b5
Matthew Song <songww1997@naver.com>
2024-09-25 11:49:20 +0900

    [KAN-68] refactor: 모임 만들기 모달 분리 (캘린더)

---
+++ src/app/components/Modal/MakeGatheringModal/Calendar.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import { useRef } from 'react';
+import DatePicker from 'react-datepicker';
+
+interface CalendarProps {
+  dateTime: Date | null;
+  setDateTime: (date: Date | null) => void;
+}
+
+const Calendar = ({ dateTime, setDateTime }: CalendarProps) => {
+  const datepickerRef = useRef(null);
+
+  return (
+    <div className='space-y-12 text-16 font-semibold'>
+      <h2>날짜</h2>
+      <div className='flex w-full items-center justify-center rounded-xl border border-var-gray-200 py-16'>
+        <div className='w-252'>
+          <DatePicker
+            id='datepicker'
+            locale='ko'
+            ref={datepickerRef}
+            dateFormat='yyyy-MM-dd'
+            selected={dateTime}
+            onChange={(date) => setDateTime(date as Date)}
+            minDate={new Date()}
+            inline
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Calendar;
