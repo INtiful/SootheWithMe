fe02d4dfbdcc0b1096c2b74526eab400e5e95b9e
chaeney <chaekey@gmail.com>
2024-09-25 17:07:21 +0900

    [KAN-74] chore : CI 에러 해결을 위해 mockData 파일 복구

---
+++ src/app/(main)/mypage/created/mockData.ts
@@ -0,0 +1,78 @@
+import { UserJoinedGatheringsData } from '@/types/data.type';
+
+export const tomorrow = new Date();
+tomorrow.setDate(tomorrow.getDate() + 1);
+
+export const registrationEnd = new Date(tomorrow);
+registrationEnd.setDate(tomorrow.getDate() - 1);
+
+export const DATA_LIST: UserJoinedGatheringsData[] = [
+  {
+    teamId: 0,
+    id: 0,
+    type: 'dalaemfit',
+    name: '달램핏 오피스 스트레칭',
+    dateTime: tomorrow.toISOString(),
+    registrationEnd: registrationEnd.toISOString(),
+    location: '을지로 3가',
+    participantCount: 20,
+    capacity: 20,
+    image: '/images/mock-image.png',
+    createdBy: 0,
+    canceledAt: null,
+    joinedAt: registrationEnd.toISOString(),
+    isCompleted: true,
+    isReviewed: false,
+  },
+  {
+    teamId: 0,
+    id: 1,
+    type: 'dalaemfit',
+    name: '달램핏 오피스 스트레칭',
+    dateTime: tomorrow.toISOString(),
+    registrationEnd: registrationEnd.toISOString(),
+    location: '을지로 3가',
+    participantCount: 12,
+    capacity: 20,
+    image: '/images/mock-image.png',
+    createdBy: 0,
+    canceledAt: null,
+    joinedAt: registrationEnd.toISOString(),
+    isCompleted: false,
+    isReviewed: false,
+  },
+  {
+    teamId: 0,
+    id: 2,
+    type: 'dalaemfit',
+    name: '달램핏 오피스 스트레칭',
+    dateTime: tomorrow.toISOString(),
+    registrationEnd: registrationEnd.toISOString(),
+    location: '을지로 3가',
+    participantCount: 12,
+    capacity: 20,
+    image: '/images/mock-image.png',
+    createdBy: 0,
+    canceledAt: null,
+    joinedAt: registrationEnd.toISOString(),
+    isCompleted: true,
+    isReviewed: true,
+  },
+  {
+    teamId: 0,
+    id: 3,
+    type: 'dalaemfit',
+    name: '달램핏 오피스 스트레칭',
+    dateTime: tomorrow.toISOString(),
+    registrationEnd: registrationEnd.toISOString(),
+    location: '을지로 3가',
+    participantCount: 12,
+    capacity: 20,
+    image: '/images/mock-image.png',
+    createdBy: 0,
+    canceledAt: null,
+    joinedAt: registrationEnd.toISOString(),
+    isCompleted: true,
+    isReviewed: false,
+  },
+];
