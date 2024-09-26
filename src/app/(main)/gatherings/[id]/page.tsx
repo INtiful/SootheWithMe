f4d845cc7a85cba960d74a99e0cb94353874695b
Homin <coc1381@naver.com>
2024-09-26 18:21:47 +0900

    [KAN-80] remove: 페이지네이션 체크 위한 mockData 제거

--- src/app/(main)/gatherings/[id]/page.tsx
+++ src/app/(main)/gatherings/[id]/page.tsx
@@ -1,8 +1,3 @@
-/**
- * TODO:
- *  - mockData 삭제
- */
-
 import GatheringDetail from './_component/GatheringDetail';
 import {
   GatheringInfoType,
@@ -37,30 +32,3 @@ const GatheringsDetailPage = async ({
 };
 
 export default GatheringsDetailPage;
-
-// mockdata
-export const mockReviewsData: ReviewsType[] = Array.from(
-  { length: 30 },
-  (_, i) => ({
-    teamId: i % 2 === 0 ? i : `T${i}`,
-    id: i + 1,
-    score: Math.floor(Math.random() * 5) + 1, // 1 ~ 5점 랜덤 점수
-    comment: `This is a review comment for review #${i + 1}.`,
-    createdAt: new Date(2024, 8, 26 - i).toISOString(), // 날짜는 9월 26일에서 하루씩 줄어듦
-    Gathering: {
-      teamId: i % 2 === 0 ? i : `T${i}`,
-      id: i + 101,
-      type: i % 3 === 0 ? 'WORKSHOP' : i % 3 === 1 ? 'MEETING' : 'EVENT',
-      name: `Gathering #${i + 1}`,
-      dateTime: new Date(2024, 9, i + 1).toISOString(), // 10월 날짜로 설정
-      location: i % 2 === 0 ? 'Seoul' : 'Busan',
-      image: `https://example.com/gathering${i + 1}.jpg`,
-    },
-    User: {
-      teamId: i % 2 === 0 ? i : `U${i}`,
-      id: i + 201,
-      name: `User #${i + 1}`,
-      image: `https://example.com/user${i + 1}.jpg`,
-    },
-  }),
-);
