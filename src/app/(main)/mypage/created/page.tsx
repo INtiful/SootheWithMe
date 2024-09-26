c3b0a1f0bdc638ecea3f1c06880825f1d5090dcb
chaeney <chaekey@gmail.com>
2024-09-26 10:41:30 +0900

    [KAN-74] feat : 유저 아이디로 client side에서 data fetch 및 loading 기능 추가

--- src/app/(main)/mypage/created/page.tsx
+++ src/app/(main)/mypage/created/page.tsx
@@ -1,15 +1,52 @@
+'use client';
+
 import getGatherings from '@/app/api/actions/gatherings/getGatherings';
 import GatheringList from './_component/GatheringList';
+import { GatheringsListData } from '@/types/data.type';
+import { useUser } from '@/app/(auth)/context/UserContext';
+import { useEffect, useState } from 'react';
+import { set } from 'zod';
+import { DATA_LIST } from './mockData';
 
-const CreatedPage = async () => {
-  // TODO : User ID를 조회
-  const USER_ID = '718';
-  const gatherings = await getGatherings({ createdBy: USER_ID });
+const CreatedPage = () => {
+  const { user } = useUser();
+  const [gatheringsList, setGatheringsList] = useState<GatheringsListData[]>(
+    [],
+  );
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchGatheringsData = async () => {
+      if (user) {
+        setIsLoading(true);
+
+        try {
+          const gatherings = await getGatherings({ createdBy: user.id });
+          // setGatheringsList(gatherings);
+          if (gatherings.length < 1) {
+            setGatheringsList(DATA_LIST);
+          } else {
+            setGatheringsList(gatherings);
+          }
+        } catch (error) {
+          console.error('Error fetching data:', error);
+        } finally {
+          setIsLoading(false); // 요청이 끝나면 로딩 상태 해제
+        }
+      }
+    };
+
+    fetchGatheringsData();
+  }, [user]);
 
   return (
     <>
-      {gatherings.length > 0 ? (
-        <GatheringList dataList={gatherings} />
+      {isLoading ? (
+        <div className='flex grow items-center justify-center text-14 font-medium text-var-gray-500'>
+          모임 정보를 불러오고 있어요...
+        </div>
+      ) : gatheringsList.length > 0 ? (
+        <GatheringList dataList={gatheringsList} />
       ) : (
         <div className='flex grow items-center justify-center text-14 font-medium text-var-gray-500'>
           아직 만든 모임이 없어요
