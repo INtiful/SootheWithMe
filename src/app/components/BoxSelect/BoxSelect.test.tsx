cc25a7c1a172a8b182a222c1cbc4177e0683cc26
Matthew Song <songww1997@naver.com>
2024-10-07 16:03:44 +0900

    [KAN-42] fix: 테스트 대상 변경 및 줄바꿈 추가

--- src/app/components/BoxSelect/BoxSelect.test.tsx
+++ src/app/components/BoxSelect/BoxSelect.test.tsx
@@ -6,8 +6,10 @@ describe('BoxSelect Component Test', () => {
   // 기본 렌더링 확인
   it('should render BoxSelect Component', () => {
     render(<BoxSelect title='제목' isSelected={false} onChange={() => {}} />);
-    const titleElement = screen.getByText('제목');
-    expect(titleElement).toBeInTheDocument();
+
+    const boxSelectElement = screen.getByTestId('box-select');
+
+    expect(boxSelectElement).toBeInTheDocument();
   });
 
   // 선택 시 색상 변경 확인
