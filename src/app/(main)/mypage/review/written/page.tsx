371ae2d9d1899f386a549d9f970445d045344d50
Matthew Song <songww1997@naver.com>
2024-10-10 16:43:30 +0900

    [KAN-113] feat: 카드 컴포넌트 Info, Image 부분을 Link 태그로 감싸기

--- src/app/(main)/mypage/review/written/page.tsx
+++ src/app/(main)/mypage/review/written/page.tsx
@@ -22,32 +22,30 @@ const Page = () => {
   });
 
   return (
-    <>
-      <div className='grow pt-16'>
-        {/* tab */}
-        <ReviewFilterTab />
+    <div className='grow pt-16'>
+      {/* tab */}
+      <ReviewFilterTab />
 
-        {/* cards */}
-        {writtenReviews?.length ? (
-          <div className='my-24 flex flex-col gap-24'>
-            {writtenReviews.map((review) => (
-              <Link href={`/gatherings/${review.Gathering.id}`} key={review.id}>
-                <Review
-                  key={review.id}
-                  rating={review.score}
-                  image_source={review.Gathering.image}
-                  description={review.comment}
-                  user_name={review.User.name}
-                  date={review.createdAt}
-                />
-              </Link>
-            ))}
-          </div>
-        ) : (
-          <EmptyReviewPage />
-        )}
-      </div>
-    </>
+      {/* cards */}
+      {writtenReviews?.length ? (
+        <div className='my-24 flex flex-col gap-24'>
+          {writtenReviews.map((review) => (
+            <Link href={`/gatherings/${review.Gathering.id}`} key={review.id}>
+              <Review
+                key={review.id}
+                rating={review.score}
+                image_source={review.Gathering.image}
+                description={review.comment}
+                user_name={review.User.name}
+                date={review.createdAt}
+              />
+            </Link>
+          ))}
+        </div>
+      ) : (
+        <EmptyReviewPage />
+      )}
+    </div>
   );
 };
 
