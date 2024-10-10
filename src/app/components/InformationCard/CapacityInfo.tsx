4801e82d31f82a3e0766820874e768db0ca5f9b9
Homin <coc1381@naver.com>
2024-10-10 17:45:14 +0900

    [KAN-118] refactor: InformationCard 컴포넌트 분리

---
+++ src/app/components/InformationCard/CapacityInfo.tsx
@@ -0,0 +1,31 @@
+import ProgressBar from '../ProgressBar/ProgressBar';
+import { MIN_PARTICIPANTS } from '@/constants/common';
+
+interface CapacityInfoProps {
+  participantCount: number;
+  maxParticipants: number;
+}
+
+const CapacityInfo = ({
+  participantCount,
+  maxParticipants,
+}: CapacityInfoProps) => {
+  return (
+    <div>
+      <ProgressBar
+        participantNumber={participantCount}
+        hasParticipantNumber={false}
+        hasOpeningConfirmed={false}
+        capacity={maxParticipants}
+        hasText={false}
+      />
+
+      <div className='mt-10 flex justify-between text-12 font-semibold'>
+        <div>최소 인원 {MIN_PARTICIPANTS}명</div>
+        <div className='text-var-orange-500'>최대 인원 {maxParticipants}명</div>
+      </div>
+    </div>
+  );
+};
+
+export default CapacityInfo;
