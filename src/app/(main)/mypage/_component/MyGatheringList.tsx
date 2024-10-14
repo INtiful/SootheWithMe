b2bb3a540e896a219bdf4e26b97feab19496397a
Matthew Song <songww1997@naver.com>
2024-10-14 16:38:10 +0900

    [KAN-120] fix: 적용하지 않은 부분에 코드 수정하기

--- src/app/(main)/mypage/_component/MyGatheringList.tsx
+++ src/app/(main)/mypage/_component/MyGatheringList.tsx
@@ -5,7 +5,7 @@ import Card from '@/app/components/Card/Card';
 import InfiniteScroll from '@/app/components/InfiniteScroll/InfiniteScroll';
 import ReviewModal from '@/app/components/Modal/ReviewModal';
 import useParticipation from '@/hooks/useParticipation';
-import { joinedKeys } from '@/queries/mypage/joined';
+import { queries } from '@/queries';
 import { UserData } from '@/types/client.type';
 import { UserJoinedGatheringsData } from '@/types/data.type';
 import { useState } from 'react';
@@ -33,7 +33,7 @@ const MyGatheringList = ({ initData, user }: MyGatheringListProps) => {
     <>
       <InfiniteScroll
         initData={initData}
-        queryKey={joinedKeys._def}
+        queryKey={queries.joined._def}
         queryFn={getMyGatherings}
         emptyText='아직 참여한 모임이 없습니다.'
         errorText='모임을 불러오지 못했습니다.'
@@ -45,7 +45,7 @@ const MyGatheringList = ({ initData, user }: MyGatheringListProps) => {
               handleButtonClick={() => {
                 item.isCompleted
                   ? handleOpenModal(item.id)
-                  : handleWithdrawClickWithId(item.id, joinedKeys._def);
+                  : handleWithdrawClickWithId(item.id, queries.joined._def);
               }}
             />
           </Card>
