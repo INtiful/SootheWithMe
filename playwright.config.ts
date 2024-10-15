dacdf5bdcee127c8edbe689d90aac853d3b0acb2
chaeney <chaekey@gmail.com>
2024-10-15 10:19:59 +0900

    [KAN-122] chore : playwright 테스트 CI workflow 추가

--- playwright.config.ts
+++ playwright.config.ts
@@ -72,8 +72,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    // command: 'cross-env PLAYWRIGHT_TEST=true npm run start',
-    command: 'cross-env PLAYWRIGHT_TEST=true npm run dev',
+    command: 'cross-env PLAYWRIGHT_TEST=true npm run start',
     url: 'http://127.0.0.1:3000',
     reuseExistingServer: !process.env.CI,
   },
