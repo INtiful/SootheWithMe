460e742e8049c4e8630c3946cd96e3be79c487de
Homin <coc1381@naver.com>
2024-10-15 09:22:39 +0900

    [KAN-121] fix: 남은 일자가 음수로 나오는 버그 수정

--- src/app/(main)/gatherings/[id]/_component/GatheringImage.tsx
+++ src/app/(main)/gatherings/[id]/_component/GatheringImage.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image';
 
 import { IconAlarm } from '@/public/icons';
 import getDaysUntilRegistrationEnd from '@/utils/getDaysUntilRegistrationEnd';
+import getTagMessage from '@/utils/getTagMessage';
 
 interface GatheringImageProps {
   image: string;
@@ -10,6 +11,7 @@ interface GatheringImageProps {
 
 const GatheringImage = ({ image, endTime }: GatheringImageProps) => {
   const daysLeft = endTime ? getDaysUntilRegistrationEnd(endTime) : null;
+  const tagMessage = getTagMessage(daysLeft, endTime);
 
   return (
     <div className='relative h-[270px] w-full md:w-[50vw] lg:max-w-[486px]'>
@@ -24,11 +26,7 @@ const GatheringImage = ({ image, endTime }: GatheringImageProps) => {
       {daysLeft !== null && daysLeft <= 7 && (
         <div className='absolute right-0 top-0 flex items-center gap-4 rounded-bl-[12px] rounded-tr-[20px] bg-orange-600 py-2 pl-4 pr-6 text-xs font-medium text-var-white'>
           <IconAlarm width='24' height='24' />
-          {daysLeft === 0 ? (
-            <span className='text-12 font-semibold'>오늘 {endTime}시 마감</span>
-          ) : (
-            <span className='text-12 font-semibold'>{daysLeft}일 후 마감</span>
-          )}
+          <span className='text-12 font-semibold'>{tagMessage}</span>
         </div>
       )}
     </div>
