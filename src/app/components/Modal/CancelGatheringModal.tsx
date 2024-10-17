a4bb2e00e8db12fc95f81fd3b9968fd2370e543b
hakyoung12 <gkr097@naver.com>
2024-10-17 11:34:50 +0900

    [KAN-132] feat: 모임취소 재확인 모달 추가

---
+++ src/app/components/Modal/CancelGatheringModal.tsx
@@ -0,0 +1,47 @@
+'use client';
+
+import Button from '../Button/Button';
+import ModalFrame from './ModalFrame';
+import ModalHeader from './ModalHeader';
+
+interface CancelGatheringModalProps {
+  onClose: () => void;
+  onSubmit?: () => void;
+}
+
+const CancelGatheringModal = ({
+  onClose,
+  onSubmit,
+}: CancelGatheringModalProps) => {
+  return (
+    <ModalFrame onClose={onClose}>
+      <div className='flex max-h-328 w-320 flex-col gap-24 rounded-xl bg-var-white p-24 md:w-440 dark:border dark:border-neutral-600 dark:bg-neutral-800'>
+        <ModalHeader title={''} onClose={onClose} />
+        <div className='text-center text-16 font-medium'>
+          모임을 정말 취소하시겠습니까?
+        </div>
+        <div className='flex items-center gap-16'>
+          {/* 버튼 그룹 */}
+          <div className={'flex w-full justify-center gap-8'}>
+            <div className='w-120'>
+              <Button
+                name='취소'
+                variant='white'
+                onClick={() => console.log('모임취소')}
+              />
+            </div>
+            <div className='w-120'>
+              <Button
+                name='확인'
+                variant='default'
+                onClick={() => console.log('닫기')}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </ModalFrame>
+  );
+};
+
+export default CancelGatheringModal;
